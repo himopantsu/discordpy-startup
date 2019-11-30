4735ce2d18cb005e8ca03f3215acccd0d3d40532
himopantsu <57516419+himopantsu@users.noreply.github.com>
2019-11-30 11:10:14 +0900

    Update Document2.py

--- Document2.py
+++ Document2.py
@@ -133,8 +133,8 @@ async def on_message(message):
 			await message.channel.send(f"リアクションおしてくれると助かります:macs: ")
 			await message.channel.send(f"残りの今シーズンも頑張りましょう:daynogal:")
 			for i in range(14):
-				q = await message.channel.send(f"{(day+timedelta(days=i)).month}/{(day+timedelta(days=i)).day}({youbi[i]})")
-				await q.add_reaction('⭕')
+				await message.channel.send(f"{(day+timedelta(days=i)).month}/{(day+timedelta(days=i)).day}({youbi[i]})")
+				#await q.add_reaction('⭕')
 		else:return
 	
 	elif message.content == "やるじゃん":
