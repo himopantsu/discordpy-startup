4c0a34f6b431718d9e8fd31b5ea09f3342334475
himopantsu <57516419+himopantsu@users.noreply.github.com>
2019-12-02 23:26:39 +0900

    Update Document2.py

--- Document2.py
+++ Document2.py
@@ -26,8 +26,9 @@ client = discord.Client()  # 接続に使用するオブジェクト
 credentials = ServiceAccountCredentials.from_json_keyfile_name('okashi-55fd53c0b60c.json', scope)
 gc = gspread.authorize(credentials)
 SPREADSHEET_KEY = sheet_token
-worksheet = gc.open_by_key(SPREADSHEET_KEY)
-worksheet_list = worksheet.worksheets()
+workbook = gc.open_by_key(SPREADSHEET_KEY)
+worksheet = workbook.worksheet("参加者")
+#worksheet_list = worksheet.worksheets()
 print(worksheet_list)
 #worksheet = gc.open_by_url("https://docs.google.com/spreadsheets/d/"+SPREADSHEET_KEY+"/edit#gid=0")
 
